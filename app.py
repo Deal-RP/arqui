43757b882c0efa76042852150724b062aadf85cc
Derly Rodas <derly.rodas@gmail.com>
2021-04-27 20:22:30 -0600

    V.1

--- app.py
+++ app.py
@@ -21,7 +21,6 @@ def lab10():
 @app.route('/lab10Pi', methods =["GET", "POST"])
 def lab10Pi():
     if request.method == "POST":
-        entrada = request.form.get("entrada")
         #last_name = request.form.get("lname")
         if entrada is not None:
             return jsonify({ 'r': entrada }), 201
