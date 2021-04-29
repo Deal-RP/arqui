938155bdc2eb656ad6ea934800726bb8731092db
Derly Rodas <derly.rodas@gmail.com>
2021-04-28 18:34:24 -0600

    V.1

--- app.py
+++ app.py
@@ -8,23 +8,63 @@ from json2html import *
 app = Flask(__name__)
 
 entrada = None
+codigo = ''
+entradaPi = ''
+numeroPi = 0
+total = 0
+
+def codigo(){
+    global codigo,total
+    if total == 0:
+        codigo = '1111110'
+    if total == 1:
+        codigo = '0110000'
+    if total == 2:
+        codigo = '1101101'
+    if total == 3:
+        codigo = '1111001'
+    if total == 4:
+        codigo = '0110011'
+    if total == 5:
+        codigo = '1011011'
+    if total == 6:
+        codigo = '1011111'
+    if total == 7:
+        codigo = '1110000'
+    if total == 8:
+        codigo = '1111111'
+    if total == 9:
+        codigo = '1110011'
+}
 
 #LAB-10
 @app.route('/lab10', methods =["GET", "POST"])
 def lab10():
     if request.method == "POST":
-        global entrada
+        global entrada, entradaPi
         entrada = request.form.get("entrada")
-        #last_name = request.form.get("lname")
-    return render_template("entrada.html")
+    return render_template("entrada.html", data=entradaPi)
 
 @app.route('/lab10Pi', methods =["GET", "POST"])
 def lab10Pi():
-    #if request.method == "POST":
-        #last_name = request.form.get("lname")
-    if entrada is not None:
-        return jsonify({ 'r': entrada }), 201
-    return jsonify({ 'r': '1' }), 201
+    if request.method == "POST":
+        resp = request.get_json()
+        global entradaPi, entrada, total
+        entradaPi = resp['pi']
+        total = int(str(entradaPi), 2) - int(entrada)
+        if entrada is not None:
+            extra = '0'
+            aux = total
+            if total >= 10:
+                total = total - 10
+                extra = '1'
+            codigo()
+            return jsonify({ 
+                'r': extra,
+                'display': codigo,
+                'total': aux
+            }), 201
+    return jsonify({ 'r': '0' }), 201
 
 
 # Post
